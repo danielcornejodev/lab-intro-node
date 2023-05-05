eaf772e5e07bfa8a483b706933a22b57d3a44e97
Daniel Cornejo <contact@danielcornejo.dev>
2023-05-05 16:34:00 -0400

    add method complete

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ class SortedList {
 
   add(item) {
     if (!item) {
-      throw new Error('Item cannot be null or undefined');
+      throw new Error('OutOfBounds');
     }
 
     let index = this.items.findIndex((element) => element >= item);
 
+    //In most languages, -1 is a number that cannot represent an array index. Therefore, if .findIndex() method returns -1 it means that there is no such element for sure
     if (index === -1) {
       index = this.items.length;
     }
 
+    //injecting the item being a number in this case at the end if does not exist, otherwise, gets injected at position of first occurance.  
     this.items.splice(index, 0, item);
     this.length = this.items.length;
   }
