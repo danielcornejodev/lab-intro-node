f9c4c86a8e17653996e967e3eeb9c8c57005fb36
Daniel Cornejo <contact@danielcornejo.dev>
2023-05-04 22:02:37 -0400

    first test complete

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  add(item) {}
+  add(item) {
+    if (!item) {
+      throw new Error('Item cannot be null or undefined');
+    }
 
-  get(pos) {}
+    let index = this.items.findIndex((element) => element >= item);
+
+    if (index === -1) {
+      index = this.items.length;
+    }
+
+    this.items.splice(index, 0, item);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+
+  }
 
   max() {}
 
